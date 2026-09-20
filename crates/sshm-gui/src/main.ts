941f50a601277b7ecb5cbf184f8a3887c7323da0
Sn0wAlice <snow.alice@pm.me>
2026-09-20 14:22:12 +0200

    purge

--- crates/sshm-gui/src/main.ts
+++
@@ -1,9 +0,0 @@
-import "./app.css";
-import App from "./App.svelte";
-
-const target = document.getElementById("app");
-if (!target) throw new Error("#app mount point missing");
-
-const app = new App({ target });
-
-export default app;
